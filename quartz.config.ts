6626e25e5c11e0abf054e2a056cd6fc83ed586d4
JorenJ2 <jorenbelt@gmail.com>
2026-01-19 12:25:16 +0100

    update

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "In the name of war",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
